3a26cad7ebcbbe66327b90f961d3552395ec4ca5
Bakhtawar khan <160532125+baki1996@users.noreply.github.com>
2024-10-29 23:42:01 +0500

    push my code

---
+++ figma-to-next.js/src/app/components/Posts.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import React from 'react'
+import SinglePost from './SinglePost'
+
+const Posts = () => {
+  return (
+    <div className='w-full h-auto md:h-[502px] bg-pink-100 pb-10 px-4 md:px-60'>
+      <div className='flex justify-between py-6'>
+        <h5 className='font-heebo font-semibold text-lg md:text-[22px]'>Recent Posts</h5>
+        <Link className='font-heebo font-semibold text-lg md:text-[22px] text-red-300' href={"/posts"}>View all</Link>
+      </div>
+      <div className='flex flex-col md:flex-row justify-center gap-5'>
+        <SinglePost />
+        <SinglePost />
+      </div>
+    </div>
+  )
+}
+
+export default Posts
